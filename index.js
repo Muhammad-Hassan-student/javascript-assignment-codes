890cb2d7bb5bc399391e4141f04811979c63fefa
Muhammad-Hassan-student <153417305+Muhammad-Hassan-student@users.noreply.github.com>
2023-12-24 13:49:13 +0500

    assignment javascript codes practise yourselt

---
+++ index.js
@@ -0,0 +1,169 @@
+//if statement
+// let age=5;
+// if(age >18){
+//     console.log("you can drive");
+// }
+// else{
+//     console.log("beta jao phele cnic bnwao");
+// }
+
+//if practise yourself
+
+// let age=prompt("whats your age?:");
+// if(age >= 18 && age <= 30 ){
+//     alert("you can get admission");
+// }
+// else {
+//     alert("you cannot get admission");
+// }
+
+
+// let time=prompt("hey whats the time");
+// if(time > 17 && time < 21 ){
+//     alert("good evening");
+// }
+// else if (time > 12 && time < 17){
+//     alert("good afternoon");
+// }
+// else if(time >5 && time <12  ){
+//     alert("good morning");
+// }
+// else{
+//     alert("good night")
+// }
+
+//switch statement
+
+
+
+// alert("welcome to my shop");
+
+// let mobiles=prompt("mobile type");
+// switch(mobiles){
+//     case 'Samsung A32':
+//         alert("price 70,000pk");
+//         break;
+//     case 'i phone 15 pro max':
+//         alert("price 800,000pk");
+//         break;
+//     case  'oppo f21':
+//         alert("price48,000");
+//         break;
+//     default:
+//     alert(`${mobiles} aaj khatam hogai kl ana. `);
+// }
+
+
+// let fruits=prompt("konsa fruit chahiye");
+// switch(fruits){
+//     case 'apple':
+//         alert("100Rs kg ");
+//         break;
+//     case 'orange':
+//         alert("150Rs kg");
+//         break;
+//     case 'grapes':
+//         alert("60rs kg");
+//         break;
+//     case 'mango':
+//         alert("abhi season nh mango ka");
+//         break;
+//     case 'pinapple':
+//         alert("190Rs kg");
+//         break;
+//     default:
+//         alert(`${fruits} abhi khtm hogai kl ayega`);
+// }
+// alert("or koi hukum ");
+
+
+//for loop 
+// let print=0;
+// for(print= 0;print < 1000;print++){
+//     console.log("my name is hassan");
+// }
+
+// let a=2;
+// for(i=0;i <= 10; i++){
+//     console.log(a*i);
+// }
+
+// let films= {
+//     name:"ironman",
+//     industry:"hollywood",
+//     filmsmadeby:"marvel"
+//  }
+// for (const key in films) {
+    
+//         const element = films[key];
+//         console.log(key,element)
+    
+    
+// // }
+
+// for (const a of "ironman") {
+//     console.log(a);
+    
+// }
+
+// let i=10;
+// while(i < 25){
+//    console.log(i);
+//    i++;
+// }
+
+
+//functions
+
+
+// function nice(name){
+//    console.log("hey!" + name + " you are good student")
+//    console.log("hey!" + name + " you are good boy")
+//    console.log("hey!" + name + " you are good teacher")
+// }
+// nice("hassan")
+
+function sum(a,b){
+    console.log(a+b)
+}
+sum(8,2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// functions learning with abbas cr
+
+//1 )regular function
+
+function heroes(){
+   console.log("my name is muhammad hassan");
+}
+heroes()
+
+//2) 
+
